8cc040708535fc226546494f3420374ccf382d32
Felipe Cespedes <felipe.cespedes@protonmail.com>
2021-06-27 15:02:43 -0400

    configured tags and added scratchpad

--- config.def.h
+++ config.def.h
@@ -31,14 +31,16 @@ typedef struct {
 	const char *name;
 	const void *cmd;
 } Sp;
-const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
-const char *spcmd2[] = {"st", "-n", "spfm", "-g", "144x41", "-e", "ranger", NULL };
+const char *spcmd1[] = {"st", "-n", "spterm", "-g", "110x24", NULL };
+const char *spcmd2[] = {"st", "-n", "spfm", "-g", "110x24", "-e", "lf", NULL };
 const char *spcmd3[] = {"keepassxc", NULL };
+const char *spcmd4[] = {"pavucontrol", NULL };
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"spterm",      spcmd1},
 	{"spranger",    spcmd2},
 	{"keepassxc",   spcmd3},
+	{"pavucontrol", spcmd4},
 };
 
 /* tagging */
@@ -48,12 +50,16 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",	  NULL,			NULL,		0,				1,			 -1 },
-	{ "Firefox",      NULL,			NULL,		1 << 8,				0,			 -1 },
-	{ NULL,		  "spterm",		NULL,		SPTAG(0),			1,			 -1 },
-	{ NULL,		  "spfm",		NULL,		SPTAG(1),			1,			 -1 },
-	{ NULL,		  "keepassxc",		NULL,		SPTAG(2),			0,			 -1 },
+	/* class          instance              title           tags mask              isfloating   monitor */
+	{ "Gimp",	  NULL,			NULL,		0,				1,	-1 },
+	{ "Firefox",      NULL,			NULL,		1 << 0,				0,	-1 },
+	{ "discord",	  NULL,			NULL,		1 << 1,				0,	-1 },
+	{ "Steam",	  NULL,			NULL,		1 << 8,				0,	-1 },
+	{ "xournalpp",	  NULL, 		NULL, 		1 << 2, 			0,	-1 },
+	{ NULL,		  "spterm",		NULL,		SPTAG(0),			1,	-1 },
+	{ NULL,		  "spfm",		NULL,		SPTAG(1),			1,	-1 },
+	{ NULL,		  "keepassxc",		NULL,		SPTAG(2),			1,	-1 },
+	{ NULL,           "pavucontrol",	NULL,		SPTAG(3),			1,      -1 },
 };
 
 /* layout(s) */
@@ -143,6 +149,7 @@ static Key keys[] = {
 	{ MODKEY,       		XK_s,  	   togglescratch,  {.ui = 0 } },
 	{ MODKEY,            		XK_u,	   togglescratch,  {.ui = 1 } },
 	{ MODKEY,            		XK_x,	   togglescratch,  {.ui = 2 } },
+	{ MODKEY,            		XK_v,	   togglescratch,  {.ui = 3 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
